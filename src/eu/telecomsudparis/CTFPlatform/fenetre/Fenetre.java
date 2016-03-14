cf1e03f49cdbd5cae5cf9a65ac6e0baeae590745
sarlam <sarlam@minet.net>
2016-03-14 22:58:33 +0100

    commit du code crade

--- src/eu/telecomsudparis/CTFPlatform/fenetre/Fenetre.java
+++ src/eu/telecomsudparis/CTFPlatform/fenetre/Fenetre.java
@@ -14,7 +14,7 @@ import javax.swing.JPanel;
  */
 public class Fenetre extends JFrame {
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	//private eu.telecomsudparis.CTFPlatform.epreuve.generator.helper.Panneau panneau = new eu.telecomsudparis.CTFPlatform.epreuve.generator.helper.Panneau();
@@ -27,12 +27,12 @@ public class Fenetre extends JFrame {
 	private JButton bouton6 = new JButton("Epreuve de eu.telecomsudparis.CTFPlatform.epreuve.generator.JavaScript");
 	private JButton bouton7 = new JButton("Epreuve de logique n�1");
 	private JButton bouton8 = new JButton("Epreuve de logique n�2");
-	
+
 	//private JLabel label = new JLabel("");
 	private JPanel pan2 = new JPanel();
 	int i = 0;
 	int j = 0;
-	
+
 	public Fenetre(){
 		this.setTitle("Plateforme de cr�ation d'�preuves CTF");
 		this.setSize(600, 500);
@@ -40,7 +40,7 @@ public class Fenetre extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(true);
 		this.setAlwaysOnTop(true);
-		
+
 		bouton.addActionListener(new BoutonListener());
 		bouton2.addActionListener(new Bouton2Listener());
 		bouton3.addActionListener(new Bouton3Listener());
@@ -49,18 +49,18 @@ public class Fenetre extends JFrame {
 		bouton6.addActionListener(new Bouton6Listener());
 		bouton7.addActionListener(new Bouton7Listener());
 		bouton8.addActionListener(new Bouton8Listener());
-		
+
 		GridLayout g = new GridLayout();
 		g.setColumns(2);
 		g.setRows(4);
 		g.setHgap(5);
 		g.setVgap(5);
-		
+
 		pan2.setLayout(g);
 		//pan.add(panneau, BorderLayout.CENTER);
-		
+
 		//label.setHorizontalAlignment(JLabel.CENTER);
-		
+
 		pan2.add(bouton);
 		pan2.add(bouton2);
 		pan2.add(bouton3);
@@ -69,84 +69,129 @@ public class Fenetre extends JFrame {
 		pan2.add(bouton6);
 		pan2.add(bouton7);
 		pan2.add(bouton8);
-		
+
 		//pan.add(label);
 		//pan.add(pan2);
 
-		this.setContentPane(pan2);
-		
-		this.setVisible(true);
-	}
-	
-	class BoutonListener implements ActionListener {
-		
-		public void actionPerformed(ActionEvent e) {
-			dispose();
-			new FenetreCrypto();
-		}
-		
-	}
-	
-	class Bouton2Listener implements ActionListener {
-		
-		public void actionPerformed(ActionEvent e) {
-			dispose();
-			new FenetreStegano();
-		}
-		
-	}
-	
-	class Bouton3Listener implements ActionListener {
-		
-		public void actionPerformed(ActionEvent e) {
-			dispose();
-			new FenetreStegano2();
-		}
-		
-	}
+    //private JLabel label = new JLabel("");
+    private JPanel pan2 = new JPanel();
+    int i = 0;
+    int j = 0;
 
-	class Bouton4Listener implements ActionListener {
-		
-		public void actionPerformed(ActionEvent e) {
-			dispose();
-			new FenetreJavaScript();
-		}
-	}
-	
-	class Bouton5Listener implements ActionListener {
-		
-		public void actionPerformed(ActionEvent e) {
-			dispose();
-			new FenetreCrack();
-		}
-	}
-	
-	
-	class Bouton6Listener implements ActionListener {
-		
-		public void actionPerformed(ActionEvent e) {
-			dispose();
-			new FenetreJavaScript2();
-		}
-	}
+    public Fenetre() {
+        this.setTitle("Plateforme de cr�ation d'�preuves CTF");
+        this.setSize(600, 500);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(true);
+        this.setAlwaysOnTop(true);
 
-	
-	class Bouton7Listener implements ActionListener {
-		
-		public void actionPerformed(ActionEvent e) {
-			dispose();
-			new FenetreLogique();
-		}
-		
-	}
-	
-	class Bouton8Listener implements ActionListener {
-		
-		public void actionPerformed(ActionEvent e) {
-			dispose();
-			new FenetreLogique2();
-		}
-		
-	}
-	
+        bouton.addActionListener(new BoutonListener());
+        bouton2.addActionListener(new Bouton2Listener());
+        bouton3.addActionListener(new Bouton3Listener());
+        bouton4.addActionListener(new Bouton4Listener());
+        bouton5.addActionListener(new Bouton5Listener());
+        bouton6.addActionListener(new Bouton6Listener());
+        bouton7.addActionListener(new Bouton7Listener());
+        bouton8.addActionListener(new Bouton8Listener());
+
+        GridLayout g = new GridLayout();
+        g.setColumns(2);
+        g.setRows(4);
+        g.setHgap(5);
+        g.setVgap(5);
+
+        pan2.setLayout(g);
+        //pan.add(panneau, BorderLayout.CENTER);
+
+        //label.setHorizontalAlignment(JLabel.CENTER);
+
+        pan2.add(bouton);
+        pan2.add(bouton2);
+        pan2.add(bouton3);
+        pan2.add(bouton4);
+        pan2.add(bouton5);
+        pan2.add(bouton6);
+        pan2.add(bouton7);
+        pan2.add(bouton8);
+
+        //pan.add(label);
+        //pan.add(pan2);
+
+        this.setContentPane(pan2);
+
+        this.setVisible(true);
+    }
+
+    class BoutonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            dispose();
+            new FenetreCrypto();
+        }
+
+    }
+
+    class Bouton2Listener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            dispose();
+            new FenetreStegano();
+        }
+
+    }
+
+    class Bouton3Listener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            dispose();
+            new FenetreStegano2();
+        }
+
+    }
+
+    class Bouton4Listener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            dispose();
+            new FenetreJavaScript();
+        }
+    }
+
+    class Bouton5Listener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            dispose();
+            new FenetreCrack();
+        }
+    }
+
+
+    class Bouton6Listener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            dispose();
+            new FenetreJavaScript2();
+        }
+    }
+
+
+    class Bouton7Listener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            dispose();
+            new FenetreLogique();
+        }
+
+    }
+
+    class Bouton8Listener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            dispose();
+            new FenetreLogique2();
+        }
+
+    }
+
 }
